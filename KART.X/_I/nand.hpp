f01912453e6662246551650788392ff685d75fa8
OxLxExG <oooeivas@gmail.com>
2026-01-12 15:03:21 +0500

    del kart

--- KART.X/_I/nand.hpp
+++
@@ -1,9 +0,0 @@
-#pragma  once
-#include <avr/io.h>
-#include <Data.h>
-
-extern bool NAND_ReadBadBlocks(void* n, uint8_t cnt);
-extern bool NAND_ReadModel(void* n);
-extern bool NAND_Check(void);
-
-
